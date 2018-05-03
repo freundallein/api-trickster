9d8e98005de5848fc495268839fb4a1a072cd54f
S.Zhavoronkov <freund.allein@gmail.com>
2018-05-03 17:09:42 +0300

    update

--- trickster/api_requests.py
+++ trickster/api_requests.py
@@ -16,12 +16,14 @@ async def obtain_buses(session, service_url, auth_params):
     api_url = f"{service_url}/Line/Mode/bus?{auth_params}"
     try:
         async with session.get(api_url) as resp:
+            print("Buses")
             print(resp.status)
             json_response = await resp.json()
-            print(json_response)
             buses = json.loads(json_response)
+            print(buses)
             return buses
     except Exception as err:
+        print("Buses")
         print(err)
         raise err
 
@@ -31,32 +33,31 @@ async def obtain_stops(session, service_url, auth_params, lines):
         async for api_url, line in generate_stops_url(lines, service_url,
                                                   auth_params):
             async with session.get(api_url) as resp:
+                print("Stops")
                 print(resp.status)
                 json_response = await resp.json()
                 stops = json.loads(json_response)
+                print(stops)
                 yield stops, line
     except Exception as err:
+        print("Stops")
         print(err)
         raise err
 
 
 async def obtain_arrivals(session, service_url, auth_params, lines):
     lines = await aggregate_lines_by_20(lines)
-    async for api_url, buses in generate_arrivals_url(lines, service_url,
-                                                      auth_params):
-        print(f"Arrivals for: {buses}")
-        await asyncio.sleep(1)
-        #     print(buses)
-        #     async with session.get(api_url) as resp:
-        #         print(resp.status)
-        #         print(await resp.text())
-        #         await asyncio.sleep(1)
-        with open('test_fixtures/arrivals.json', 'r') as file:
-            json_response = file.read()
-            try:
+    try:
+        async for api_url, buses in generate_arrivals_url(lines, service_url,
+                                                          auth_params):
+            # print(f"Arrivals for: {buses}")
+            async with session.get(api_url) as resp:
+                print("Arrivals")
+                print(resp.status)
+                json_response = await resp.json()
                 arrivals = json.loads(json_response)
-                buses = ['134', '135', '136']
                 yield arrivals, buses
-            except Exception as err:
-                print(err)
-                raise err
+    except Exception as err:
+        print("Arrivals")
+        print(err)
+        raise err
